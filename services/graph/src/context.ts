a176551809d539abe35b2841be57c58b55ac4208
olliwh <98967791+olliwh@users.noreply.github.com>
2026-04-07 17:31:00 +0200

    removed unused variable

--- services/graph/src/context.ts
+++ services/graph/src/context.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import sqlite from "better-sqlite3";
 
-// const betterSqlite = new sqlite("./prisma/library.db");
 const adapter = new PrismaBetterSqlite3({
   url: "file:./prisma/library.db",
 });
